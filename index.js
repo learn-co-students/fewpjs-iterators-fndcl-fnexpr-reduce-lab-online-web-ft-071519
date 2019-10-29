1ed19d0e862ad01fc7b927f4feb6e6419edb4d5f
BaronLip <baronlip@gmail.com>
2019-10-28 23:28:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Non reduce method:
+// function addBatteries(batteryBatches) {
+//     const totalBatteries = 0
+
+//     batteryBatches.forEach( batteryBatch => {
+//         totalBatteries += batteryBatch
+//     })
+//     return totalBatteries
+// }
+
+function addBatteries(total, eachAdditional) {
+    total = total + eachAdditional;
+    return total
+}
+
+const totalBatteries = batteryBatches.reduce(addBatteries, 0)
+
+// When calling reduce on batteryBatches, the argument addBatteries is called. 
+// "total" becomes the accumulator.
+// "eachAdditional" becomes the current value.
